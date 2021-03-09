0e017df5a0cc0501e1b24bff3a687f7c4d4698e1
Florian Bruggisser <florian@bildspur.ch>
2021-03-09 18:27:31 +0100

    added wifi setup block

---
+++ blockly/blocks/arduino/wifi.js
@@ -0,0 +1,39 @@
+/**
+ * @license Licensed under the Apache License, Version 2.0 (the "License"):
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+/**
+ * @fileoverview Blocks for the Arduino wifi communcations.
+ *     The Arduino built in functions syntax can be found at:
+ *     http://arduino.cc/en/Reference/HomePage
+ *
+ */
+'use strict';
+
+goog.provide('Blockly.Blocks.wifi');
+
+goog.require('Blockly.Blocks');
+goog.require('Blockly.Types');
+
+
+/** Common HSV hue for all blocks in this category. */
+Blockly.Blocks.serial.HUE = 20;
+
+Blockly.Blocks['wifi_setup'] = {
+  /**
+   * Block for setting the speed of the WIFI connection.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.setHelpUrl('https://www.arduino.cc/en/Reference/WiFiNINABegin');
+    this.setColour(Blockly.Blocks.serial.HUE);
+    this.appendDummyInput()
+        .appendField("Setup WiFi with ssid: ")
+        .appendField(new Blockly.FieldTextInput(""), 'SSID')
+        .appendField(" and password: ")
+        .appendField(new Blockly.FieldTextInput(""), 'PASSWORD')
+    this.setInputsInline(true);
+    this.setTooltip("Begin wifi connection with specified credentials.");
+  }
+};
---
+++ blockly/generators/arduino/wifi.js
@@ -0,0 +1,33 @@
+/**
+ * @license Licensed under the Apache License, Version 2.0 (the "License"):
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+/**
+ * @fileoverview Code generator for the Arduino wifi blocks.
+ *     Arduino wifi library docs: https://www.arduino.cc/en/Reference/WiFiNINABegin
+ *
+ */
+'use strict';
+
+goog.provide('Blockly.Arduino.wifi');
+
+goog.require('Blockly.Arduino');
+
+
+/**
+ * Code generator for block for setting up the wifi connection
+ * @param {!Blockly.Block} block Block to generate the code from.
+ * @return {array} Completed code.
+ */
+Blockly.Arduino['wifi_setup'] = function(block) {
+  Blockly.Arduino.addInclude('spi', '#include <SPI.h>');
+  Blockly.Arduino.addInclude('wifiNina', '#include <WiFiNINA.h>');
+
+  var ssid = block.getFieldValue('SSID');
+  var password = block.getFieldValue('PASSWORD');
+  var wifiSetupCode = `WiFi.begin("${ssid}", "${password}");`;
+  Blockly.Arduino.addSetup('wifi', wifiSetupCode, true);
+  var code = '';
+  return code;
+};
