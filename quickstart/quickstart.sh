f3f45b9e98559a27b7f52bd6e22ea25a1598e4aa
Florian Bruggisser <florian@bildspur.ch>
2021-03-19 16:11:11 +0100

    fixed the zip download and clojure link

--- quickstart/quickstart.sh
+++ quickstart/quickstart.sh
@@ -4,25 +4,32 @@ echo "installing ardublockly..."
 ROOT_DIR="ardublockly"
 
 unzip-from-link() {
- local download_link=$1; shift || return 1
- local temporary_dir
+  local download_link=$1; shift || return 1
+  local extraction_directory=$2; shift || return 1
 
- temporary_dir=$(mktemp -d) \
- && curl -LO "${download_link:-}" \
- && unzip -d "$temporary_dir" \*.zip \
- && rm -rf \*.zip \
- && mv "$temporary_dir"/* ${1:-"$HOME/Downloads"} \
- && rm -rf $temporary_dir
+  curl -L "$download_link" --output temp.zip &> /dev/null
+  unzip -qq -d "$extraction_directory" temp.zip
+  rm temp.zip
 }
 
 # remove already existing version
 if [ -d "$root" ]; then rm -Rf $root; fi
 
 # download latest release
-unzip-from-link https://github.com/IAD-ZHDK/ardublockly/archive/master.zip "$PWD/$ROOT_DIR"
-rm master.zip
+echo "downloading ardublockly..."
+unzip-from-link https://github.com/IAD-ZHDK/ardublockly/archive/master.zip "$PWD/"
+mv "ardublockly-master" "$ROOT_DIR"
 
 pushd "$ROOT_DIR"
-git submodule update --init --recursive
+
+# setup closure
+echo "downloading closure library..."
+rm -rf "closure-library"
+unzip-from-link https://github.com/google/closure-library/archive/refs/tags/v20161024.zip "$PWD"
+mv "closure-library-20161024" "closure-library"
+
+# start
+echo "starting..."
 python start.py
+
 popd
